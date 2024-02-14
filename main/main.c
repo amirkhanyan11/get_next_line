c7d931f3b6c69ae43d06095a143185b22ed6fcfb
Artyom Amirkhanyan <aamirkha@c2r6s1.42yerevan.am>
2024-02-14 19:18:15 +0400

    overrated as fuck

--- main/main.c
+++ main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kali <kali@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:58:49 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/02/14 02:17:44 by kali             ###   ########.fr       */
+/*   Updated: 2024/02/14 17:45:58 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,27 +19,24 @@ void gnl(int fd, char * str)
 
 
 	line = get_next_line(fd);
-//	printf("%s", line);
+	//printf("%s", line);
 	if (!line && !str)
 		printf("1 & NULLIK\n");
 	else
-		printf("%d\n", !strcmp(line, str));
+		printf("%d\n", strcmp(line, str));
 	free(line);
 }
 
 int main()
 {
-	int fd = open("foo.txt", O_RDONLY);
+	int fd = open("/Users/aamirkha/Desktop/get_next_line/trip/files/multiple_line_no_nl", O_RDONLY);
 
-	gnl(fd, "aaaa");
-	gnl(fd, "aaaa");
-
-	static int *a;
-
-	int *p = malloc(4);
-
-
-	a = p;
+	gnl(fd, "01234567890123456789012345678901234567890\n");
+	gnl(fd, "987654321098765432109876543210987654321098\n");
+	gnl(fd, "0123456789012345678901234567890123456789012\n");
+	gnl(fd, "987654321098765432109876543210987654321098\n");
+	gnl(fd, "01234567890123456789012345678901234567890");
+	gnl(fd, NULL);
 
 	close(fd);
 	//system("leaks a.out");
